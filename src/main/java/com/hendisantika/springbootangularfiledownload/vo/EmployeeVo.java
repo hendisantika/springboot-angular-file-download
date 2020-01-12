1cdc688d666b2739d6e6f6cede10c21f4bdb4c28
Hendi Santika <hendisantika@gmail.com>
2020-01-13 03:23:35 +0700

    #6 - Add EmployeeVo

---
+++ src/main/java/com/hendisantika/springbootangularfiledownload/vo/EmployeeVo.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootangularfiledownload.vo;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-angular-file-download
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/01/20
+ * Time: 03.22
+ */
+@Data
+public class EmployeeVo {
+    private Integer id;
+    private String name;
+    private String email;
+}
