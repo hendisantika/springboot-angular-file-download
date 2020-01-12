2e842297cce16f446df805d084fdc6a39c8af6aa
Hendi Santika <hendisantika@gmail.com>
2020-01-13 03:27:49 +0700

    #11 - Add downloadFile API in EmployeeRestController

--- src/main/java/com/hendisantika/springbootangularfiledownload/controller/EmployeeRestController.java
+++ src/main/java/com/hendisantika/springbootangularfiledownload/controller/EmployeeRestController.java
@@ -1,9 +1,19 @@
 package com.hendisantika.springbootangularfiledownload.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hendisantika.springbootangularfiledownload.service.EmployeeService;
+import com.hendisantika.springbootangularfiledownload.vo.EmployeeVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,9 +25,25 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 03.26
  */
 @RestController
+@RequestMapping(value = "/employees")
 @CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeRestController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> downloadFile() throws Exception {
+        List<EmployeeVo> employees = employeeService.getEmployeeList();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(employees);
+        byte[] isr = json.getBytes();
+        String fileName = "employees.json";
+        HttpHeaders respHeaders = new HttpHeaders();
+        respHeaders.setContentLength(isr.length);
+        respHeaders.setContentType(new MediaType("text", "json"));
+        respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+        respHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+        return new ResponseEntity<byte[]>(isr, respHeaders, HttpStatus.OK);
+    }
 }
