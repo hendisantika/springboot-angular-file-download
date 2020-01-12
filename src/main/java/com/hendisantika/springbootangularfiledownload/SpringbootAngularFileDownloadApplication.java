aafffca4d6df09368ff423e16666269338192416
Hendi Santika <hendisantika@gmail.com>
2020-01-13 03:14:23 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootangularfiledownload/SpringbootAngularFileDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootangularfiledownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularFileDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAngularFileDownloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootangularfiledownload/SpringbootAngularFileDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootangularfiledownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAngularFileDownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
