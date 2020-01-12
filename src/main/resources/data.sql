78baa3e3d484bbe817caf3d7b01fa00bfb6ef74d
Hendi Santika <hendisantika@gmail.com>
2020-01-13 03:20:40 +0700

    #4 - Add sql script file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into employee(name,email_address)
+values('naruto','naruto@konohagakure.co.jp');
+insert into employee(name,email_address)
+values('sasuke','sasuke@konohagakure.co.jp');
+insert into employee(name,email_address)
+values('sakura','sakura@konohagakure.co.jp');
+insert into employee(name,email_address)
+values('kakashi','kakashi@konohagakure.co.jp');
