c3c6d90fb7e596319ec8ad31ba254bd2ad7920c8
Hendi Santika <hendisantika@gmail.com>
2020-03-03 20:05:49 +0700

    #12 - Add Employee & EmployeeVo Entity

--- src/main/java/com/hendisantika/springbootangularfiledownload/entity/Employee.java
+++ src/main/java/com/hendisantika/springbootangularfiledownload/entity/Employee.java
@@ -2,7 +2,11 @@ package com.hendisantika.springbootangularfiledownload.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +26,38 @@ public class Employee {
     private String name;
     @Column(name = "email_address")
     private String email;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
--- src/main/java/com/hendisantika/springbootangularfiledownload/vo/EmployeeVo.java
+++ src/main/java/com/hendisantika/springbootangularfiledownload/vo/EmployeeVo.java
@@ -1,6 +1,10 @@
 package com.hendisantika.springbootangularfiledownload.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,8 +16,36 @@ import lombok.Data;
  * Time: 03.22
  */
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeVo {
     private Integer id;
     private String name;
     private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
